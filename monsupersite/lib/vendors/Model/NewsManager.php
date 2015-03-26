c684e6ab4dd0f2a4d962ff655ae89291304f6afa
jondzaghe <j.ondzaghe@dreamcentury.com>
2015-03-26 10:08:21 +0100

    Udate: Adding abstract function modify

--- monsupersite/lib/vendors/Model/NewsManager.php
+++ monsupersite/lib/vendors/Model/NewsManager.php
@@ -2,6 +2,7 @@
 namespace Model;
 
 use \OCFram\Manager;
+use \Entity\News;
 
 abstract class NewsManager extends Manager
 {
@@ -22,4 +23,39 @@ abstract class NewsManager extends Manager
   abstract public function getUnique($id);
 
   abstract public function count();
+
+
+  /**
+   * Méthode permettant d'ajouter une news.
+   * @param $news News La news à ajouter
+   * @return void
+   */
+  abstract protected function add(News $news);
+  
+  /**
+   * Méthode permettant d'enregistrer une news.
+   * @param $news News la news à enregistrer
+   * @see self::add()
+   * @see self::modify()
+   * @return void
+   */
+  public function save(News $news)
+  {
+    if ($news->isValid())
+    {
+      $news->isNew() ? $this->add($news) : $this->modify($news);
+    }
+    else
+    {
+      throw new \RuntimeException('La news doit être validée pour être enregistrée');
+    }
+  }
+
+
+  /**
+   * Méthode permettant de modifier une news.
+   * @param $news news la news à modifier
+   * @return void
+   */
+  abstract protected function modify(News $news);
 }
