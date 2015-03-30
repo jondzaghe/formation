4a4e4a3bd2369a526c52df3a563bd97e45429d5b
jondzaghe <j.ondzaghe@dreamcentury.com>
2015-03-30 11:03:48 +0200

    Adding signin formbuilder

---
+++ monsupersite/lib/vendors/FormBuilder/SigninFormBuilder.php
@@ -0,0 +1,47 @@
+<?php
+namespace FormBuilder;
+ 
+use \OCFram\FormBuilder;
+use \OCFram\StringField;
+use \OCFram\TextField;
+use \OCFram\MaxLengthValidator;
+use \OCFram\NotNullValidator;
+
+ 
+class SigninFormBuilder extends FormBuilder{
+
+	public function build(){
+
+	    $this->form->add(new StringField([
+	        'label' => 'Nom (login)',
+	        'name' => 'fucNom',
+	        'type' => 'text',
+	        'maxLength' => 50,
+	        'validators' => [
+	          new MaxLengthValidator('Le nom spécifié est trop long (20 caractères maximum)', 20),
+	          new NotNullValidator('Merci de spécifier un nom'),
+	        ],
+	       ]))
+	       ->add(new StringField([
+	        'label' => 'Prenom',
+	        'name' => 'fucPrenom',
+	        'type' => 'text',
+	        'maxLength' => 50,
+	        'validators' => [
+	          new MaxLengthValidator('Le prenom spécifié est trop long (20 caractères maximum)', 20),
+	          new NotNullValidator('Merci de spécifier un prenom'),
+	        ],
+	       ]))
+	       ->add(new StringField([
+	        'label' => 'Mot de passe',
+	        'name' => 'fucMdp',
+	        'type' => 'password',
+	        'maxLength' => 50,
+	        'type' => 'password',
+	        'validators' => [
+	          new MaxLengthValidator('Le prenom spécifié est trop long (20 caractères maximum)', 20),
+	          new NotNullValidator('Merci de spécifier un mot de passe'),
+	        ],
+	       ]));
+  }
+}
