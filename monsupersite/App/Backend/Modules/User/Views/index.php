bdc0572ca38e07559b417beb5aa07ccfb7b7b0ff
jondzaghe <j.ondzaghe@dreamcentury.com>
2015-03-30 15:39:38 +0200

    Update: adding delete user link

--- monsupersite/App/Backend/Modules/User/Views/index.php
+++ monsupersite/App/Backend/Modules/User/Views/index.php
@@ -1,11 +1,11 @@
 <p style="text-align: center">Liste des ecrivains enregistrés</p>
  
 <table>
-  <tr><th>Nom</th><th>Prenom</th></tr>
+  <tr><th>Nom</th><th>Prenom</th><th>Actions</th></tr>
 <?php
 foreach ($listeEcrivain as $liste)
 {
-  echo '<tr><td>', $liste->fucNom(), '</td><td>', $liste->fucPrenom(), '</td></tr>', "\n";
+  echo '<tr><td>', $liste->fucNom(), '</td><td>', $liste->fucPrenom(), '</td><td><a href="user-delete-', $liste->fucId(), '.html"><img src="/images/delete.png" alt="Supprimer" /></a></td></tr>', "\n";
 }
 
 ?>
